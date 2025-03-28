536bb6a53a985d73893c882df6c7daf4cdd66a25
CGalang-9R <galang.chm@obmontessori.edu.ph>
2025-03-28 09:08:16 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function power(){
+	let var1 = parseInt(document.getElementById('varX').value);
+	window.alert('The answer is ' + Math.pow(var1, 2));
+
+}
+
+function sr(){
+	let var1 = document.getElementById('varX').value;
+	window.alert('The answer is ' + Math.sqrt(var1));
+
+}
+
+function rnd(){
+	let var1 = document.getElementById('varX').value;
+	window.alert('The answer is ' + Math.round(var1));
+
+}
+
+function rdm(){
+	let var1 = document.getElementById('varX').value;
+	window.alert('The answer is ' + Math.random(var1));
+
+}
